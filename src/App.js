1fac89320e72ad78b1ae0849917178a1ada186b0
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2023-11-19 19:12:40 +0100

    Update UI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Make any changes and push to master branch.          
         </p>
         <a
           className="App-link"
