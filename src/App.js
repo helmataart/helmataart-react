253c0d4f51a38c2093d2865c71ec1a2be4d64fda
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-01-31 23:32:54 +0100

    Add hero component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './components/hero';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Make any changes and push to master branch. That is awesome.        
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          For test
-        </a>
-      </header>
-    </div>
+    <Hero />
   );
 }
 
