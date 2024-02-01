39440b4cbc7f66cd95751f8792cf9678d1ee4e1f
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-02-01 15:08:03 +0100

    Add statsCounter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Hero from './components/hero';
 import About from './components/about';
 import Main from './components/main';
 import React from 'react';
+import WhyUs from './components/whyUs';
+import StatsCounter from './components/statsCounter';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Hero />
       <Main>
         <About />
+        <WhyUs />
+        <StatsCounter />
       </Main>
     </React.Fragment>
   );
